ed820ba0af377f0b00f3971f8dce5e782e99321f
Toby Scott <toby@tobyscott.dev>
2025-10-26 09:17:43 +1100

    Add LazyVim keybindings

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.del("n", "<leader>l")
+-- vim.keymap.del("n", "<leader>L")
+
+vim.keymap.set("n", "<leader>l", "", { desc = "LazyVim" })
+vim.keymap.set("n", "<leader>ll", "<Cmd>Lazy<CR>", { desc = "Lazy" })
+vim.keymap.set("n", "<leader>le", "<Cmd>LazyExtras<CR>", { desc = "Lazy Extras" })
+vim.keymap.set(
+  "n",
+  "<leader>lc",
+  ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+  { desc = "Configuration" }
+)
+vim.keymap.set("n", "<leader>lC", function()
+  vim.cmd.news.changelog()
+end, { desc = "Changelog" })
