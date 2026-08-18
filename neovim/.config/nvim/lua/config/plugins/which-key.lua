29f82ae35fabc0112c16e5dc02684d650e43d722
Toby Scott <toby@tobyscott.dev>
2026-08-18 17:21:38 +1000

    fix which-key menu

--- neovim/.config/nvim/lua/config/plugins/which-key.lua
+++ neovim/.config/nvim/lua/config/plugins/which-key.lua
@@ -7,14 +7,14 @@ wk.setup({
 })
 
 wk.add({
+	{ "<leader>b", group = "buffers" },
 	{ "<leader>c", group = "code" },
 	{ "<leader>cl", group = "calls" },
 	{ "<leader>d", group = "diagnostics" },
+	{ "<leader>f", group = "files/find" },
 	{ "<leader>g", group = "git" },
-	{ "<leader>h", group = "hunks" },
 	{ "<leader>s", group = "search" },
 	{ "<leader>t", group = "test" },
 	{ "<leader>u", group = "ui" },
-	{ "<leader>q", group = "quit/todos" },
-	{ "<leader>b", group = "buffers" },
+	{ "<leader>x", group = "diagnostics/quickfix" },
 })
