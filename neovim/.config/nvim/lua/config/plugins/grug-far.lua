fa3c2919db67c691491eef642d7b23c975732ffc
Toby Scott <toby@tobyscott.dev>
2026-09-17 15:26:21 +1000

    Use full vim keymap functions

--- neovim/.config/nvim/lua/config/plugins/grug-far.lua
+++ neovim/.config/nvim/lua/config/plugins/grug-far.lua
@@ -1,4 +1,3 @@
-local map = vim.keymap.set
 local gf = require("grug-far")
 
 gf.setup({
@@ -6,14 +5,14 @@ gf.setup({
 	windowCreationCommand = "split",
 })
 
-map("n", "<leader>sr", function()
+vim.keymap.set("n", "<leader>sr", function()
 	gf.open({ prefills = { search = vim.fn.expand("<cword>") } })
 end, { silent = true, desc = "Search/replace" })
 
-map("x", "<leader>sr", function()
+vim.keymap.set("x", "<leader>sr", function()
 	gf.with_visual_selection({ prefills = { paths = vim.fn.expand("%") } })
 end, { silent = true, desc = "Search/replace selection" })
 
-map("n", "<leader>sR", function()
+vim.keymap.set("n", "<leader>sR", function()
 	gf.open({ prefills = { paths = vim.fn.expand("%") } })
 end, { silent = true, desc = "Seach/replace in file" })
--- neovim/.config/nvim/lua/config/plugins/neotest.lua
+++ neovim/.config/nvim/lua/config/plugins/neotest.lua
@@ -1,4 +1,3 @@
-local map = vim.keymap.set
 local neotest = require("neotest")
 
 ---@diagnostic disable-next-line: missing-fields
@@ -30,37 +29,37 @@ neotest.setup({
 	},
 })
 
-map("n", "<leader>tr", function()
+vim.keymap.set("n", "<leader>tr", function()
 	neotest.run.run()
 end, { silent = true, desc = "Run nearest test" })
-map("n", "<leader>tf", function()
+vim.keymap.set("n", "<leader>tf", function()
 	neotest.run.run(vim.fn.expand("%"))
 end, { silent = true, desc = "Run test file" })
-map("n", "<leader>ta", function()
+vim.keymap.set("n", "<leader>ta", function()
 	---@type neotest.run.UserRunArgs
 	local args = { suite = true }
 	neotest.run.run(args)
 end, { silent = true, desc = "Run all tests" })
-map("n", "<leader>tl", function()
+vim.keymap.set("n", "<leader>tl", function()
 	neotest.run.run_last()
 end, { silent = true, desc = "Run last test" })
-map("n", "<leader>ts", function()
+vim.keymap.set("n", "<leader>ts", function()
 	neotest.summary.toggle()
 end, { silent = true, desc = "Toggle test summary" })
-map("n", "<leader>to", function()
+vim.keymap.set("n", "<leader>to", function()
 	neotest.output.open({ enter = true })
 end, { silent = true, desc = "Show test output" })
-map("n", "<leader>tO", function()
+vim.keymap.set("n", "<leader>tO", function()
 	neotest.output_panel.toggle()
 end, { silent = true, desc = "Toggle test output panel" })
-map("n", "<leader>td", function()
+vim.keymap.set("n", "<leader>td", function()
 	---@type neotest.run.UserRunArgs
 	local args = { strategy = "dap" }
 	neotest.run.run(args)
 end, { silent = true, desc = "Debug nearest test" })
-map("n", "<leader>tx", function()
+vim.keymap.set("n", "<leader>tx", function()
 	neotest.run.stop()
 end, { silent = true, desc = "Stop test run" })
-map("n", "<leader>tw", function()
+vim.keymap.set("n", "<leader>tw", function()
 	neotest.watch.toggle(vim.fn.expand("%"))
 end, { silent = true, desc = "Watch test file" })
