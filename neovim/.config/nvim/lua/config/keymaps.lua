de5d722e397d6329dcf080013dc75823e00f18cb
Toby Scott <toby@tobyscott.dev>
2026-09-18 16:18:35 +1000

    Disable arrow keys

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,9 @@ end, "Delete buffer")
 map("n", "<leader>ch", function()
 	vim.cmd("checkhealth")
 end, "Check health")
+
+-- Be a good little vimmer
+map("n", "<left>", "<cmd>echo 'Use h to move!!'<CR>")
+map("n", "<right>", "<cmd>echo 'Use l to move!!'<CR>")
+map("n", "<up>", "<cmd>echo 'Use k to move!!'<CR>")
+map("n", "<down>", "<cmd>echo 'Use j to move!!'<CR>")
