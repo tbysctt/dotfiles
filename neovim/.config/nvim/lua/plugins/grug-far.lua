df79144b79d53eba29085b17fca141c5f6334a3d
Toby Scott <toby@tobyscott.dev>
2026-09-21 22:44:52 +1000

    GrugFar: Use transient buffers, filetype filesFilter

--- neovim/.config/nvim/lua/plugins/grug-far.lua
+++ neovim/.config/nvim/lua/plugins/grug-far.lua
@@ -1,20 +1,26 @@
 require("grug-far").setup({
+	headerMaxWidth = 80,
 	engines = {
 		ripgrep = {
 			extraArgs = "--hidden",
 		},
 	},
-	windowCreationCommand = "split",
+	windowCreationCommand = "split", -- keep your horizontal split preference
 })
 
-vim.keymap.set("n", "<leader>sr", function()
-	require("grug-far").open({ prefills = { search = vim.fn.expand("<cword>") } })
-end, { silent = true, desc = "Search/replace" })
-
-vim.keymap.set("x", "<leader>sr", function()
-	require("grug-far").with_visual_selection({ prefills = { paths = vim.fn.expand("%") } })
-end, { silent = true, desc = "Search/replace selection" })
+vim.keymap.set({ "n", "x" }, "<leader>sr", function()
+	local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+	require("grug-far").open({
+		transient = true,
+		prefills = {
+			filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+		},
+	})
+end, { silent = true, desc = "Search and Replace" })
 
 vim.keymap.set("n", "<leader>sR", function()
-	require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
+	require("grug-far").open({
+		transient = true,
+		prefills = { paths = vim.fn.expand("%") },
+	})
 end, { silent = true, desc = "Search/replace in file" })
